6531414e4130afdd28082cb45f11c80abafaaa4b
Daniel Fernandes Vieira <danielvieiraufc@gmail.com>
2022-08-15 16:00:50 -0300

    refact: add useNavigate in Footer button

--- src/components/Footer/index.test.tsx
+++ src/components/Footer/index.test.tsx
@@ -1,13 +1,54 @@
-import { render, screen } from "@testing-library/react"
+import { fireEvent, render, screen } from "@testing-library/react"
 import { RecoilRoot } from "recoil"
 import Footer from "."
+import { useUsersList } from "../../state/hooks/useUsersList"
 
-describe('when users list is blank', () => {
+jest.mock('../../state/hooks/useUsersList', () => {
+  return { useUsersList: jest.fn() }
+})
+
+const mockNavigation = jest.fn();
+jest.mock('react-router-dom', () => {
+  return { useNavigate: () => mockNavigation }
+})
+
+
+describe('when it is less than 3 users in list', () => {
+  beforeEach(() => {
+    (useUsersList as jest.Mock).mockReturnValue([])
+  })
+
   test('should button is disabled', () => {
     render(<RecoilRoot><Footer /></RecoilRoot>)
 
     const button = screen.getByRole('button');
 
     expect(button).toBeDisabled();
+  })
+})
+
+describe('when it has 3 or more user in list', () => {
+  const users = ['Daniel', 'Vieira', 'Fernandes'];
+
+  beforeEach(() => {
+    (useUsersList as jest.Mock).mockReturnValue(users)
+  })
+
+  test('should button was enabled', () => {
+    render(<RecoilRoot><Footer /></RecoilRoot>)
+
+    const button = screen.getByRole('button');
+
+    expect(button).not.toBeDisabled();
+  })
+
+  test('should button redirect to corretly page', () => {
+    render(<RecoilRoot><Footer /></RecoilRoot>)
+    
+    const button = screen.getByRole('button');
+    fireEvent.click(button);
+
+    expect(mockNavigation).toHaveBeenCalledTimes(1);
+    expect(mockNavigation).toHaveBeenCalledWith('/draw');
   })
 })
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,14 +1,22 @@
-import React from 'react'
-import { useUsersList } from '../../state/hooks/useUsersList'
+import { useNavigate } from 'react-router-dom';
+import { useUsersList } from '../../state/hooks/useUsersList';
 
 const Footer = () => {
   const usersList = useUsersList();
+  const navigateTo = useNavigate()
+
+  const start = () => { navigateTo('/draw') }
 
   return (
     <footer>
-      <button disabled={usersList.length < 3}>Iniciar brincadeira!</button>
+      <button
+        disabled={usersList.length < 3}
+        onClick={start}
+      >
+        Iniciar brincadeira!
+      </button>
     </footer>
   )
 }
 
-export default Footer
+export default Footer;
