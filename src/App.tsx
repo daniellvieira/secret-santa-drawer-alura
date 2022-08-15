6264d63d6b76687ae9a89a32e58368b49c98a9f3
Daniel Fernandes Vieira <danielvieiraufc@gmail.com>
2022-08-15 14:30:05 -0300

    feat: add recoil and react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { RecoilRoot } from "recoil";
+import Form from "./components/Form";
 
 function App() {
   return (
-    <h1>Sorteador de amigo secreto</h1>
+    <BrowserRouter>
+      <RecoilRoot>
+        <Routes>
+          <Route path='/' element={Form} />
+        </Routes>
+      </RecoilRoot>
+    </BrowserRouter>
   );
 }
 
