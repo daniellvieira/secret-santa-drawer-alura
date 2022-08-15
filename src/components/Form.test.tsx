dba9c08a785571e7f2f05d66a8211802274f92e1
Daniel Fernandes Vieira <danielvieiraufc@gmail.com>
2022-08-15 14:23:42 -0300

    feat: create header

--- src/components/Form.test.tsx
+++ src/components/Form.test.tsx
@@ -1,6 +1,13 @@
 import { render, screen } from "@testing-library/react";
 import React from "react";
-// Jest
+import Form from "./Form";
+
+// AAA (Arrange, Act and Assert em inglês).
+test('um nome que que descreve o que vamos testar', () => {
+  // arrumamos o cenário (por exemplo, renderizar um componente, buscamos componentes)
+  // agimos (realizamos clicks, definimos valores)
+  // afirmamos o que queremos (onde realizamos as expectativas)
+})
 
 test('when input in blank, new user cannot be added', () => {
   render(<Form />)
@@ -13,3 +20,4 @@ test('when input in blank, new user cannot be added', () => {
   // ensure button to be disabled
   expect(button).toBeDisabled();
 })
+
